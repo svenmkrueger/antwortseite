0241eaa4b10a0988ce252695649ab759b0c151b6
svenmkrueger <svenmkrueger@gmx.de>
2026-08-31 09:08:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * ========================================================================== */
 
 // Bei JEDEM Release zusammen mit SEITEN_VERSION in seite.js hochzählen.
-const CACHE = "antwortseite-v11";
+const CACHE = "antwortseite-v12";
 const DATEIEN = [
   "./",
   "./index.html",
